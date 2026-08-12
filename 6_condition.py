77ac49c3bec078c46e3f149b434af77eb3996025
18751858284 <shizhouhu7949_xm@cicdi.cn>
2026-08-12 17:45:29 +0800

    commit

--- 6_condition.py
+++ 6_condition.py
@@ -7,4 +7,17 @@
 #     print('nonono')
 
 user_weight = float(input("体重"))
-user_height=  float(input("身高"))
+user_height = float(input("身高"))
+
+# 逻辑与：and
+# 逻辑或：or
+if user_height > 80 and user_height < 120 or user_weight == 90:
+    print("haha")
+    if user_weight > 80:
+        print("fat boy")
+    else:
+        print("slim boy")
+elif user_height > 60:
+    print("nonon")
+else:
+    print("lalalal")
